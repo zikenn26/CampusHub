8f2e6c0f7beedd75e4c94e6b921950e3606f34d2
zikenn26 <gulnayak1206@gmail.com>
2025-11-30 22:08:50 +0530

    Add pre-start script for migrate and collectstatic

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn campus_hub.wsgi:application --bind 0.0.0.0:$PORT
-
+web: python campus_hub/run_before_start.py && gunicorn campus_hub.wsgi:application --bind 0.0.0.0:$PORT
--- campus_hub/run_before_start.py
+++ campus_hub/run_before_start.py
@@ -1,4 +1,14 @@
+# campus_hub/run_before_start.py
+
+import os
+import django
 from django.core.management import call_command
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campus_hub.settings")
+django.setup()
+
+# Run DB migrations
 call_command("migrate", interactive=False)
+
+# Collect static files
 call_command("collectstatic", interactive=False)
